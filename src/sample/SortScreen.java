a3cf333a67fbdc68f0e79a8ea6b91a02554b3579
nitin-rajesh <nitin.k.rajesh@gmail.com>
2021-07-21 12:02:12 +0530

    Sync ports entangled

--- src/sample/SortScreen.java
+++ src/sample/SortScreen.java
@@ -144,7 +144,6 @@ public class SortScreen implements Initializable {
                         arrayText.setText(Arrays.toString(temporarySharedArray));
                         portSwap = new int[]{temporarySharedArray[0]};
                         resourceSharer.sendMessage(arrayToSend(portSwap, SortStatus.PORT_UPDATE));
-                        enableSync.setSelected(false);
                         //resourceSharer = new p2pNode(portSwap[0],new BaseNode().getIPAddress());
                         break;
                     }
