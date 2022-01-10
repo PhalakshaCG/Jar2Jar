4737a3a53b6da9693b014c9fd6d3c788ab5a9cb7
nitin-rajesh <nitin.k.rajesh@gmail.com>
2022-01-10 11:48:04 +0530

    Maven build configured

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,9 @@
+import sample.Main;
+
+//This class is only for platform dependent fat jars
+//Use sample.Main for all other situations
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
